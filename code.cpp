4890cbf0da35d3d553933084fa9c47fc6a337678
Ashutosh Gupta <ashusanugupta999@gmail.com>
2023-06-20 00:06:48 +0530

    third

--- code.cpp
+++ code.cpp
@@ -1,31 +1,130 @@
-#include <iostream>
-#include <string>
+#include <stdio.h>
+#include <stdbool.h>
 
-int noneRepeat(std::string S) {
-    for (int i = 0; i < S.length(); i++) {
-        char ch = S[i];
-        int flag = 0;
+#define MAX_PROCESSES 10
+#define MAX_RESOURCES 10
 
-        for (int j = 0; j < S.length(); j++){
-            if (ch == S[j] && j != i) {
-                flag = 0;
-                break;
-            } else {
-                flag = 1;
-            }
-        }
+int available[MAX_RESOURCES];
+int maximum[MAX_PROCESSES][MAX_RESOURCES];
+int allocation[MAX_PROCESSES][MAX_RESOURCES];
+int need[MAX_PROCESSES][MAX_RESOURCES];
+bool finish[MAX_PROCESSES];
 
-        if (flag == 1)
-            return i;
+bool is_safe(int process_count, int resource_count) {
+    int work[MAX_RESOURCES];
+    for (int i = 0; i < resource_count; i++) {
+        work[i] = available[i];
     }
 
-    return -1;
+    bool found;
+    do {
+        found = false;
+        for (int i = 0; i < process_count; i++) {
+            if (!finish[i]) {
+                bool can_allocate = true;
+                for (int j = 0; j < resource_count; j++) {
+                    if (need[i][j] > work[j]) {
+                        can_allocate = false;
+                        break;
+                    }
+                }
+
+                if (can_allocate) {
+                    for (int j = 0; j < resource_count; j++) {
+                        work[j] += allocation[i][j];
+                    }
+                    finish[i] = true;
+                    found = true;
+                }
+            }
+        }
+    } while (found);
+
+    for (int i = 0; i < process_count; i++) {
+        if (!finish[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool request_resources(int process_count, int resource_count, int process, int request[]) {
+    for (int i = 0; i < resource_count; i++) {
+        if (request[i] > need[process][i] || request[i] > available[i]) {
+            return false;
+        }
+    }
+
+    for (int i = 0; i < resource_count; i++) {
+        available[i] -= request[i];
+        allocation[process][i] += request[i];
+        need[process][i] -= request[i];
+    }
+
+    if (!is_safe(process_count, resource_count)) {
+        for (int i = 0; i < resource_count; i++) {
+            available[i] += request[i];
+            allocation[process][i] -= request[i];
+            need[process][i] += request[i];
+        }
+        return false;
+    }
+
+    return true;
+}
+
+bool release_resources(int process_count, int resource_count, int process, int release[]) {
+    for (int i = 0; i < resource_count; i++) {
+        if (release[i] > allocation[process][i]) {
+            return false;
+        }
+    }
+
+    for (int i = 0; i < resource_count; i++) {
+        available[i] += release[i];
+        allocation[process][i] -= release[i];
+        need[process][i] += release[i];
+    }
+
+    return true;
 }
 
 int main() {
-    std::string st = "abracadara";
+    int process_count, resource_count;
 
-    std::cout << noneRepeat(st);
+    printf("Enter the number of processes: ");
+    scanf("%d", &process_count);
+
+    printf("Enter the number of resources: ");
+    scanf("%d", &resource_count);
+
+    printf("Enter the available resources:\n");
+    for (int i = 0; i < resource_count; i++) {
+        scanf("%d", &available[i]);
+    }
+
+    printf("Enter the maximum resource need for each process:\n");
+    for (int i = 0; i < process_count; i++) {
+        for (int j = 0; j < resource_count; j++) {
+            scanf("%d", &maximum[i][j]);
+        }
+    }
+
+    printf("Enter the allocated resources for each process:\n");
+    for (int i = 0; i < process_count; i++) {
+        for (int j = 0; j < resource_count; j++) {
+            scanf("%d", &allocation[i][j]);
+            need[i][j] = maximum[i][j] - allocation[i][j];
+        }
+        finish[i] = false;
+    }
+
+    if (is_safe(process_count, resource_count)) {
+        printf("The system is in a safe state.\n");
+    } else {
+        printf("The system is not in a safe state.\n");
+    }
 
     return 0;
 }
---
+++ weather.py
@@ -0,0 +1,35 @@
+import requests
+
+# Prompt the user to enter a city name
+city = input("Enter the name of a city: ")
+
+# Replace the placeholder value with your actual API key
+API_KEY = "2606f769271b8d545fe3458b2b72ed9f"
+
+# Construct the URL with the city name and API key
+url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}"
+
+try:
+    # Send the GET request to the API
+    response = requests.get(url)
+    data = response.json()
+
+   # print(data)
+
+    # Extract relevant information from the response
+    temperature = data['main']['temp']
+    humidity = data['main']['humidity']
+    description = data['weather'][0]['description']
+    wind_speed = data['wind']['speed']
+    visibility = data['visibility']
+
+    # Display the weather information to the user
+    print(f"Weather information for {city}:")
+    print(f"Temperature: {temperature} K")
+    print(f"Humidity: {humidity}%")
+    print(f"Description: {description}")
+    print(f"Wind Speed: {wind_speed} m/s")
+    print(f"Visibility: {visibility} meters")
+    
+except requests.exceptions.RequestException as e:
+    print("An error occurred while fetching the weather data.")
